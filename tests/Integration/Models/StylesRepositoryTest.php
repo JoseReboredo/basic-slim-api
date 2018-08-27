b44586fcddc778914001f8a857bfa57bbf75daf1
Jose Reboredo <vixuesesroman@gmail.com>
2018-08-27 14:56:08 +0100

    Basic structure for the tests

---
+++ tests/Integration/Models/StylesRepositoryTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: jose
+ * Date: 27/08/18
+ * Time: 14:54
+ */
+
+namespace SlimApi\Tests\Integration\Models;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class StylesRepositoryTest
+ *
+ * @package SlimApi\Tests\Functional\Models
+ */
+class StylesRepositoryTest extends TestCase
+{
+
+}
---
+++ tests/Unit/Models/StylesRepositoryTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: jose
+ * Date: 27/08/18
+ * Time: 14:52
+ */
+
+namespace SlimApi\Tests\Unit\Models;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class StylesRepositoryTest
+ * 
+ * @package SlimApi\Tests\Unit\Models
+ */
+class StylesRepositoryTest extends TestCase
+{
+    /**
+     *
+     */
+    public function setUp()
+    {
+    }
+}
