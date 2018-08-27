271ba644240822f407a507abf39946b1e7d4692d
Jose Reboredo <vixuesesroman@gmail.com>
2018-08-27 17:49:38 +0100

    Container added to the Application

--- src/Application.php
+++ src/Application.php
@@ -9,10 +9,12 @@
 namespace SlimApi;
 
 use Pimple\Container;
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
+use Slim\Http\Request;
+use Slim\Http\Response;
 use Slim\App;
+use SlimApi\Config\Config;
 use SlimApi\DependencyInjection\ApiProvider;
+use SlimApi\Controller\StylesController;
 
 /**
  * Class Application
@@ -21,6 +23,11 @@ use SlimApi\DependencyInjection\ApiProvider;
  */
 class Application
 {
+    /**
+     * Path to the folder where we have the config files per env
+     */
+    const CONFIG_PATH = __DIR__ . '/../config/';
+
     /**
      * @var App
      */
@@ -33,13 +40,15 @@ class Application
 
     /**
      * Application constructor.
+     *
+     * @param string $param
      */
-    public function __construct()
+    public function __construct($env)
     {
-        $this->container = new Container();
+        $this->container = new Container((['config' => new Config($env, self::CONFIG_PATH)]));
         $this->container->register(new ApiProvider());
 
-        $this->slimApp = new $this->container['slim_app'];
+        $this->slimApp = $this->container['slim_app'];
     }
 
     /**
