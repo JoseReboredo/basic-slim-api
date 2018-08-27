dfc56bda3939be8c38912ba344ad13068ecd9512
Jose Reboredo <vixuesesroman@gmail.com>
2018-08-28 00:39:36 +0100

    Repository working

--- src/Models/StylesRepository.php
+++ src/Models/StylesRepository.php
@@ -8,6 +8,7 @@
 
 namespace SlimApi\Models;
 
+use MongoDB\BSON\Regex;
 use MongoDB\Collection;
 
 /**
@@ -30,15 +31,49 @@ class StylesRepository implements StylesInterface
     public function __construct(Collection $collection)
     {
         $this->collection = $collection;
+
+        $this->collection->createIndex(['tags' => 1]);
+        $this->collection->createIndex(['name' => 1]);
+        $this->collection->createIndex(['description' => 1]);
     }
 
     /**
      * Get all the available styles
      *
-     * @param array
+     * @return array
      */
-    public function getStyles()
+    public function getAllStyles()
     {
         return $this->collection->find()->toArray();
+    }
+
+    /**
+     * Get all styles that match a given tag
+     *
+     * @param string $tag
+     * @return array
+     */
+    public function getStylesByTag($tag)
+    {
+        return $this->collection->find(['tags' => new Regex('^'.$tag.'$', 'i')])->toArray();
+    }
+
+    /**
+     * Get all styles that match a search in name, tag or description
+     *
+     * @param string $value
+     * @return array
+     */
+    public function getStylesBySearch($value)
+    {
+        return $this->collection->find(
+            [
+                '$or' => [
+                    ['tags' => new Regex('^'.$value.'$', 'i')],
+                    ['name' => new Regex('^'.$value.'$', 'i')],
+                    ['search' => new Regex('^'.$value.'$', 'i')],
+                ]
+            ]
+        )->toArray();
     }
 }
