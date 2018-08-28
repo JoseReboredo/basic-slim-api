c77a316ec6065885a4bda776e1f79e052e9b130a
Jose Reboredo <vixuesesroman@gmail.com>
2018-08-28 08:36:23 +0100

    Unit tests added

--- tests/Unit/Models/StylesRepositoryTest.php
+++ tests/Unit/Models/StylesRepositoryTest.php
@@ -8,19 +8,68 @@
 
 namespace SlimApi\Tests\Unit\Models;
 
+use MongoDB\Collection;
+use MongoDB\Driver\Cursor;
 use PHPUnit\Framework\TestCase;
+use SlimApi\Models\StylesInterface;
+use SlimApi\Models\StylesRepository;
 
 /**
  * Class StylesRepositoryTest
- * 
+ *
  * @package SlimApi\Tests\Unit\Models
  */
 class StylesRepositoryTest extends TestCase
 {
     /**
-     *
+     * @var StylesInterface
+     */
+    protected $repository;
+
+    protected $collection;
+
+    /**
+     * SetUp.
      */
     public function setUp()
     {
+        $this->collection = $this->getMockBuilder(Collection::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['find', 'createIndex'])
+            ->getMock();
+        $this->collection
+            ->method('createIndex')
+            ->willReturn(true);
+
+        $this->repository = new StylesRepository($this->collection);
+    }
+
+    /**
+     * @group Models
+     */
+    public function testFormattingOfDBDocumentsWorks()
+    {
+        $expectedDocuments = [
+            [
+                "id" => "2e6c0330-bb8a-4560-aa42-ad49397ad236",
+                "name" => "Nano Hair-strokes: Full Brow",
+                "tags" => "Eyebrows,brows,perfectbrows,microblading,nanoblading,Permanentmakeup",
+                "description" => "Full brow hair-stroke application = maximum volume, definition, and symmetry.",
+                "status" => "PUBLISHED",
+                "dateCreated" => "2018-08-18 22:01:31",
+                "deleted" => 0
+            ]
+        ];
+
+        $docs = $expectedDocuments;
+        $docs[0]['tags'] = explode(',', $expectedDocuments[0]['tags']);
+
+        $this->collection
+            ->method('find')
+            ->willReturn($docs);
+
+        $result = $this->repository->getAllStyles();
+
+        $this->assertEquals($expectedDocuments, $result);
     }
 }
